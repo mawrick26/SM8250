e73d8698ddcc9dd617801c089321846def0eb91c
xNombre <kartapolska@gmail.com>
2021-09-27 15:36:47 +0200

    msm: npu: Remove debugfs
    Signed-off-by: xNombre <kartapolska@gmail.com>
    Change-Id: I2ab403b4e37b2b0717ead35c3d4166c901c66cef

--- drivers/media/platform/msm/npu/npu_hw_access.c
+++ drivers/media/platform/msm/npu/npu_hw_access.c
@@ -431,6 +431,7 @@ void subsystem_put_local(void *sub_system_handle)
 	return subsystem_put(sub_system_handle);
 }
 
+#ifdef CONFIG_DEBUG_FS
 /* -------------------------------------------------------------------------
  * Functions - Log
  * -------------------------------------------------------------------------
@@ -477,3 +478,4 @@ void npu_process_log_message(struct npu_device *npu_dev, uint32_t *message,
 	/* mutex log unlock */
 	mutex_unlock(&debugfs->log_lock);
 }
+#endif
